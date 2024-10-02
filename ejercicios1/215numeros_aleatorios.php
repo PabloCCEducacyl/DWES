14d8ef3278ea9457b2ffb479c625e96fc0cac237
Pablo Campuzano Cuadrado <pablo.camcua@educa.jcyl.es>
2024-10-02 14:10:33 +0200

    Ejercicio 215

---
+++ ejercicios1/215numeros_aleatorios.php
@@ -0,0 +1,22 @@
+<?php
+    function numeros_desordenados(){
+        $resultado = [
+            "mayor" => -1,
+            "menor" => 101,
+            "media" => 0
+        ];
+        $numeros_aleatorios = [];
+        $media_total = 0;
+        for($i = 0; $i < 33; $i++){
+            $numeros_aleatorios[$i] = random_int(0, 100);
+            if($numeros_aleatorios[$i] > $resultado["mayor"]){
+                $resultado["mayor"] = $numeros_aleatorios[$i];
+            }
+            if($numeros_aleatorios[$i] < $resultado["menor"]){
+                $resultado["menor"] = $numeros_aleatorios[$i];
+            }
+            $media_total += $numeros_aleatorios;
+        }
+        $resultado["media"] = $media_total / 33;
+        return $resultado;
+    }
