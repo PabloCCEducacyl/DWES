dacd14756f6e048e8b7104bff9c94b8c0e42bdf9
Pablo Campuzano Cuadrado <pablo.camcua@educa.jcyl.es>
2024-12-19 12:10:00 +0100

    botones cambiar formulario
    no va

--- UD4/practicaGestionAlumnos/controlador/script.js
+++ UD4/practicaGestionAlumnos/controlador/script.js
@@ -1,3 +1,21 @@
-function mostrarForm(){
+const menuBotonLogin = document.getElementById('menu-login')
+const menuBotonRegistro = document.getElementById('menu-registrar')
+const formLogin = document.getElementById('form-login')
+const formRegistro = document.getElementById('form-registro')
+
+formRegistro.classList.add('menu-escondido')
+
+menuBotonLogin.addEventListener("click", mostrarForm)
+menuBotonRegistro.addEventListener("click", mostrarForm)
+
+function mostrarForm(event){
+    let formAMostrar = event.target.parentElement.id;
     
+    if(formAMostrar == "menu-login"){
+        formLogin.classList.remove('menu-escondido')
+        formLogin.classList.add('menu-escondido')
+    } else if(formAMostrar == "menu-registro"){
+        formLogin.classList.add('menu-escondido')
+        formLogin.classList.remove('menu-escondido')
+    };
 }
