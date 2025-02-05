cf5c81cb201b0028db2147770399b69ec41ceb32
Pablo Campuzano Cuadrado <pablo.camcua@educa.jcyl.es>
2025-02-05 14:09:04 +0100

    pelicula

--- symfony/pelicula/src/Controller/PeliculaController.php
+++ symfony/pelicula/src/Controller/PeliculaController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Pelicula;
+use App\Repository\PeliculaRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/pelicula', name: 'pelicula')]
 final class PeliculaController extends AbstractController
 {
-    #[Route('/pelicula', name: 'app_pelicula')]
+
+    #[Route('/', name: 'index_pelicula')]
     public function index(): Response
     {
-        return $this->render('pelicula/index.html.twig', [
-            'controller_name' => 'PeliculaController',
-        ]);
+        return $this->json("Esta es la ruta index de pelicula");
+    }
+
+    #[Route('/lista', name: 'lista_pelicula', methods: ['GET'])]
+    public function peliculaList(EntityManagerInterface $emi): Response
+    {
+        $peliculas=$emi->getRepository(Pelicula::class)->findAll();
+        foreach($peliculas as $pelicula){
+            $peliculasJSON[] = [
+                "Titulo: {$pelicula->getTitulo()}",
+                "Descripcion: {$pelicula->getDescripccion()}",
+                "Año: {$pelicula->getAnyo()}",
+                "Duración: {$pelicula->getDuracion()}"
+            ];
+        }
+        return $this->json($peliculasJSON);
+    }
+    #[Route('/{id}', name: 'lista_pelicula', methods: ['GET'])]
+    public function peliculaVer(EntityManagerInterface $emi, int $id): Response
+    {
+        $pelicula=$emi->getRepository(Pelicula::class)->find($id);
+        if(!empty($pelicula)){
+            $peliculasJSON[] = [
+                $pelicula->getTitulo(),
+                $pelicula->getDescripccion(),
+                $pelicula->getAnyo(),
+                $pelicula->getDuracion()
+            ];
+        } else {
+            $peliculasJSON = "No encontrado";
+        }
+        return $this->json($peliculasJSON);
     }
 }
