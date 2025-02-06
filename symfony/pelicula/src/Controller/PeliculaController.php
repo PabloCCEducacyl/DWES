7526f606b3f1c6cb1c04ee42d101b7863d9d8a47
Pablo Campuzano Cuadrado <pbcampu@gmail.com>
2025-02-06 01:59:03 +0100

    peliculas

--- symfony/pelicula/src/Controller/PeliculaController.php
+++ symfony/pelicula/src/Controller/PeliculaController.php
@@ -12,7 +12,6 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/pelicula', name: 'pelicula')]
 final class PeliculaController extends AbstractController
 {
-
     #[Route('/', name: 'index_pelicula')]
     public function index(): Response
     {
@@ -22,27 +21,29 @@ final class PeliculaController extends AbstractController
     #[Route('/lista', name: 'lista_pelicula', methods: ['GET'])]
     public function peliculaList(EntityManagerInterface $emi): Response
     {
-        $peliculas=$emi->getRepository(Pelicula::class)->findAll();
-        foreach($peliculas as $pelicula){
+        $peliculas = $emi->getRepository(Pelicula::class)->findAll();
+        $peliculasJSON = [];
+        foreach ($peliculas as $pelicula) {
             $peliculasJSON[] = [
-                "Titulo: {$pelicula->getTitulo()}",
-                "Descripcion: {$pelicula->getDescripccion()}",
-                "Año: {$pelicula->getAnyo()}",
-                "Duración: {$pelicula->getDuracion()}"
+            'titulo' => $pelicula->getTitulo(),
+            'descripcion' => $pelicula->getDescripccion(),
+            'año' => $pelicula->getAnyo(),
+            'duracion' => $pelicula->getDuracion()
             ];
         }
         return $this->json($peliculasJSON);
     }
-    #[Route('/{id}', name: 'lista_pelicula', methods: ['GET'])]
+
+    #[Route('/{id}', name: 'ver_pelicula', methods: ['GET'])]
     public function peliculaVer(EntityManagerInterface $emi, int $id): Response
     {
-        $pelicula=$emi->getRepository(Pelicula::class)->find($id);
-        if(!empty($pelicula)){
-            $peliculasJSON[] = [
-                $pelicula->getTitulo(),
-                $pelicula->getDescripccion(),
-                $pelicula->getAnyo(),
-                $pelicula->getDuracion()
+        $pelicula = $emi->getRepository(Pelicula::class)->find($id);
+        if ($pelicula) {
+            $peliculasJSON = [
+                'titulo' => $pelicula->getTitulo(),
+                'descripcion' => $pelicula->getDescripccion(),
+                'año' => $pelicula->getAnyo(),
+                'duracion' => $pelicula->getDuracion()
             ];
         } else {
             $peliculasJSON = "No encontrado";
