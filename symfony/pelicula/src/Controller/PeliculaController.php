258ae64304a14ef86ffd5cf96b6d0d6b491361c0
Pablo Campuzano Cuadrado <pbcampu@gmail.com>
2025-02-09 23:20:04 +0100

    a

--- symfony/pelicula/src/Controller/PeliculaController.php
+++ symfony/pelicula/src/Controller/PeliculaController.php
@@ -3,16 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Pelicula;
-use App\Repository\PeliculaRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 #[Route('/pelicula', name: 'pelicula')]
 final class PeliculaController extends AbstractController
 {
-    #[Route('/', name: 'index_pelicula')]
+    #[Route('/', name: 'index_pelicula', methods: ['GET'])]
     public function index(): Response
     {
         return $this->json("Esta es la ruta index de pelicula");
@@ -25,10 +24,10 @@ final class PeliculaController extends AbstractController
         $peliculasJSON = [];
         foreach ($peliculas as $pelicula) {
             $peliculasJSON[] = [
-            'titulo' => $pelicula->getTitulo(),
-            'descripcion' => $pelicula->getDescripccion(),
-            'año' => $pelicula->getAnyo(),
-            'duracion' => $pelicula->getDuracion()
+                "Titulo" => $pelicula->getTitulo(),
+                "Descripcion" => $pelicula->getDescripccion(),
+                "Año" => $pelicula->getAnyo(),
+                "Duración" => $pelicula->getDuracion()
             ];
         }
         return $this->json($peliculasJSON);
@@ -40,14 +39,54 @@ final class PeliculaController extends AbstractController
         $pelicula = $emi->getRepository(Pelicula::class)->find($id);
         if ($pelicula) {
             $peliculasJSON = [
-                'titulo' => $pelicula->getTitulo(),
-                'descripcion' => $pelicula->getDescripccion(),
-                'año' => $pelicula->getAnyo(),
-                'duracion' => $pelicula->getDuracion()
+                "Titulo" => $pelicula->getTitulo(),
+                "Descripcion" => $pelicula->getDescripccion(),
+                "Año" => $pelicula->getAnyo(),
+                "Duración" => $pelicula->getDuracion()
             ];
         } else {
             $peliculasJSON = "No encontrado";
         }
         return $this->json($peliculasJSON);
+    }
+
+    #[Route('/', name: 'registrar_pelicula', methods: ['POST'])]
+    public function peliculaRegistrar(EntityManagerInterface $emi, Request $request): Response
+    {
+        //Llamada a la ruta 
+        /*
+        "titulo" : "Nueva peli",
+        "descripcion" : "Esta es la nueva peli",
+        "anyo" : 2025,
+        "duracion" : 90
+        */
+        $body=$request->getContent();
+        $data = json_decode($body, true);
+
+        $pelicula = new Pelicula();
+        $pelicula->setTitulo($data['titulo']);
+        $pelicula->setDescripccion($data['descripcion']);
+        $pelicula->setAnyo($data['anyo']);
+        $pelicula->setDuracion($data['duracion']);
+        
+        $emi->persist($pelicula);
+        $emi->flush();
+
+        return $this->json("todo correcto");
+    }
+
+    #[Route('/{id}', name: 'borrar_pelicula', methods: ['DELETE'])]
+    public function peliculaBorrar(EntityManagerInterface $emi, int $id): Response
+    {
+        $pelicula = $emi->getRepository(Pelicula::class)->find($id);
+        if(!$pelicula){
+            $respuesta = "Pelicula no existe";
+        } else {
+            $emi->remove($pelicula);
+            $emi->flush();
+            $respuesta = "Borrado correctamente";
+        }
+
+        return $this->json($respuesta);
     }
 }
